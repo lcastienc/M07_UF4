a7c2fc1c71606d4136cfcaaf0c436f873476d798
lcastienc <2023_luis.castillo@iticbcn.cat>
2024-04-05 01:15:11 +0200

    Project finished

--- TIC_GestioPersonal/centre/views.py
+++ TIC_GestioPersonal/centre/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+#First route, index_one with path ''
+def index_one(request):
+    return HttpResponse("Hello World, Bienvenidos a DJ DJango Oleee.")
+
+#Second route, with path 'about/'
+def about(request):
+    return HttpResponse("About me, sobre mi. Soy Django, un framwrok de python para crear aplicaciones webs.")
+
+#Third route, with path 'index/'
+def index(request):
+    #return HttpResponse("Hello, world")
+    professor = {"nom": "John", "cognom": "Doe", "edat": 25}
+    template = loader.get_template('index.html')
+    dades = template.render({'nombre':professor['nom'], 'cognom':professor['cognom'], 'edat':professor['edat']})
+    return HttpResponse(dades)
